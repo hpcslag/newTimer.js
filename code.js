641395d33a3c6983e10647d1dd0bd44b16beb537
MacTaylor <cslag@hotmail.com.tw>
2015-06-07 21:03:53 +0800

    Create code.js

---
+++ code.js
@@ -0,0 +1,46 @@
+function Timer(callback, time) {
+    this.setTimeout(callback, time);
+}
+
+Timer.prototype.setTimeout = function(callback, time) {
+    var self = this;
+    if(this.timer) {
+        clearTimeout(this.timer);
+    }
+    this.finished = false;
+    this.callback = callback;
+    this.time = time;
+    this.timer = setTimeout(function() {
+         self.finished = true;
+        callback();
+    }, time);
+    this.start = Date.now();
+}
+
+Timer.prototype.add = function(time) {
+   if(!this.finished) {
+       // add time to time left
+       time = this.time - (Date.now() - this.start) + time;
+       this.setTimeout(this.callback, time);
+   }
+}
+
+var timer = new Timer(function() { // init timer with 5 seconds
+    console.log("FOO");
+}, 0);
+
+timer.add(2000);
+console.log("Add timer");
+timer.add(5000);
+console.log("Add timer");
+
+var time = 1;
+
+var invtime = setInterval(function(){
+	if(time == 10){
+		clearTimeout(invtime);
+	}
+	console.log(time);
+	time++;
+},1000);
+
